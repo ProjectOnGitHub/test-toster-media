f9bd6f62c628ca5e56278461161749fb4bfccd18
Aleksandr <soft@vhlv.ru>
2023-09-20 22:53:43 +0300

    fix: timer error

--- src/index.js
+++ src/index.js
@@ -6,32 +6,36 @@ function requireAll(r) {
 
 requireAll(require.context('./svg', true, /\.svg$/));
 
-function startTimer(hours, minutes, seconds) {
-  const targetTime = new Date();
-  targetTime.setHours(hours, minutes, seconds);
+document.addEventListener('DOMContentLoaded', () => {
+  function startTimer(hours, minutes, seconds) {
+    const targetTime = new Date();
+    targetTime.setHours(hours, minutes, seconds);
 
-  function formatTime(time) {
-    const padZero = value => String(value).padStart(2, '0');
-    const hours = padZero(time.getHours());
-    const minutes = padZero(time.getMinutes());
-    const seconds = padZero(time.getSeconds());
-    return `${hours}:${minutes}:${seconds}`;
-  }
+    function formatTime(time) {
+      const padZero = value => String(value).padStart(2, '0');
+      const currentHours = padZero(time.getHours());
+      const currentMinutes = padZero(time.getMinutes());
+      const currentSeconds = padZero(time.getSeconds());
+      return `${currentHours}:${currentMinutes}:${currentSeconds}`;
+    }
 
-  function subtractOneSecond() {
-    targetTime.setSeconds(targetTime.getSeconds() - 1);
+    function subtractOneSecond() {
+      targetTime.setSeconds(targetTime.getSeconds() - 1);
+      document.querySelector('.offer__label-count').textContent = formatTime(targetTime);
+
+      if (
+        targetTime.getSeconds() === 0 &&
+        targetTime.getMinutes() === 0 &&
+        targetTime.getHours() === 0
+      ) {
+        clearInterval(timerInterval);
+      }
+    }
+
     document.querySelector('.offer__label-count').textContent = formatTime(targetTime);
 
-    if (
-      targetTime.getSeconds() === 0 &&
-      targetTime.getMinutes() === 0 &&
-      targetTime.getHours() === 0
-    ) {
-      clearInterval(timerInterval);
-    }
+    const timerInterval = setInterval(subtractOneSecond, 1000);
   }
 
-  const timerInterval = setInterval(subtractOneSecond, 1000);
-}
-
-startTimer(4, 51, 16);
+  startTimer(4, 51, 16);
+});
